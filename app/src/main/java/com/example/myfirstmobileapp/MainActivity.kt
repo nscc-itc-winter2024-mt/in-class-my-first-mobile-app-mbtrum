ee5ee8505038ab367c7baa285102dedfdbfb8760
Michael Trumbull <W0304263@campus.nscc.ca>
2024-01-12 12:32:06 -0400

    Beginning compose lessons.

--- app/src/main/java/com/example/myfirstmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstmobileapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myfirstmobileapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myfirstmobileapp.models.Message
+
 import com.example.myfirstmobileapp.ui.theme.MyFirstMobileAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,26 +23,60 @@ class MainActivity : ComponentActivity() {
             MyFirstMobileAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
+                    // Create a list of messages
+                    val messages = listOf(
+                        Message("Mike","Data classes in Kotlin are primarily used to hold data." ),
+                        Message("Mike","For each data class, the compiler automatically generates additional member functions that allow you to print an instance to readable output, compare instances, copy instances, and more. Data classes are marked with data."),
+                        Message("Mike","The compiler automatically derives the following members from all properties declared in the primary constructor"),
+                        Message("Mike","If there are explicit implementations of .equals(), .hashCode(), or .toString() in the data class body or final implementations in a superclass, then these functions are not generated, and the existing implementations are used."),
+                        Message("Mike","data class User(val name: String = \"\", val age: Int = 0)"),
+                        Message("Mike","On the JVM, if the generated class needs to have a parameterless constructor, default values for the properties have to be specified (see Constructors).")
+                        )
+
+                    //Conversation(messages)
+
+                    // Use layout composables to arrange composables on the screen
+                    Column {
+                        Text("Alfred Sisley")
+                        Text("3 minutes ago")
+
+                        Row {
+                            Text("Alfred Sisley")
+                            Text("3 minutes ago")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@Composable
+fun Conversation(messages: List<Message>) {
+    for (message in messages) {
+        MessageCard(message.author, message.body)
+    }
+}
+
 // My Greeting function.
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MessageCard(author: String, body: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = author,
+        modifier = modifier
+    )
+    Text(
+        text = body,
+        modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyFirstMobileAppTheme {
-        Greeting("Android")
-    }
-}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyFirstMobileAppTheme {
+//        Greeting("Android")
+//    }
+//}
---
+++ app/src/main/java/com/example/myfirstmobileapp/models/Message.kt
@@ -0,0 +1,7 @@
+package com.example.myfirstmobileapp.models
+
+// A Data Class to store to conversation message
+data class Message (
+    var author: String,
+    var body: String
+)
